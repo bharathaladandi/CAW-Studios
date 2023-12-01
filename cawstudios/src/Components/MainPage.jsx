c3d284d41482b6eef25a7573d8e92b258ffe6e73
bharathaladandi <bharathaladandi@gmail.com>
2023-12-02 01:16:23 +0530

    Navbar created

--- cawstudios/src/Components/MainPage.jsx
+++ cawstudios/src/Components/MainPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { OrderPage } from './OrderPage'
+import { Navbar } from './Navbar'
 
 export const MainPage = () => {
   return (
     <div>
+      <div><Navbar /></div>
         <h1>MainPage</h1>
         <OrderPage />
     </div>
---
+++ cawstudios/src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../Styles/Navbar.style.css';
+export const Navbar = () => {
+    return (
+        <div className='Container'>
+            <div className='Navinner'>
+                <div>Recco</div>
+                <div>Store</div>
+                <div>Orders</div>
+                <div>Analytics</div>
+            </div>
+            <div className='Navinnersec'>
+                <i>abcd</i>
+                <div>Hello, James</div>
+            </div>
+        </div>
+    )
+}
