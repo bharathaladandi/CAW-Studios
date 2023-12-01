f6edb19bb11b7e4940570064886d4e7fbc55320d
bharathaladandi <bharathaladandi@gmail.com>
2023-12-02 00:48:44 +0530

    folders created

---
+++ cawstudios/src/Components/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { OrderPage } from './OrderPage'
+
+export const MainPage = () => {
+  return (
+    <div>
+        <h1>MainPage</h1>
+        <OrderPage />
+    </div>
+    
+  )
+}
