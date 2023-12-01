d6bc28f8d34a49f0bc240119e5bd5da70e8c8f25
bharathaladandi <bharathaladandi@gmail.com>
2023-12-02 01:48:11 +0530

    MainPage UI created

--- cawstudios/src/Components/MainPage.jsx
+++ cawstudios/src/Components/MainPage.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import { OrderPage } from './OrderPage'
 import { Navbar } from './Navbar'
+import '../Styles/MainPage.style.css';
+
 
 export const MainPage = () => {
   return (
     <div>
       <div><Navbar /></div>
-        <h1>MainPage</h1>
-        <OrderPage />
+      <div className='maincontainer'>
+        <div className='mainorderdiv'>
+          Orders {">"} Order32457ABC 
+        </div>
+        <div className='maincontainersec'>
+          <h3>Order 32457ABC</h3>
+          <div className='mainfle'>
+            <div className='btn1'>Back</div>
+            <div className='btn2'>Approve order</div>
+          </div>
+        </div>
+      </div>
+      <div><OrderPage /></div>
+
     </div>
-    
+
   )
 }
